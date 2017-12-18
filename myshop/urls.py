0e81736f4f56422ca4eee95bb110901df11b037c
boris liu <liulijun@263.net>
2017-12-18 12:39:01 +0800

    20171218

--- myshop/urls.py
+++ myshop/urls.py
@@ -13,10 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('shop.urls', namespace = 'shop')),
-]
+] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root = settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
--- shop/views.py
+++ shop/views.py
@@ -14,15 +14,16 @@ def product_list(request, category_slug = None):
         products = products.filter(category = category)
 
     return render(request,
-                    'shop/product/list.html',
+                    'product/list.html',
                     {'category': category,
                     'categories': categories,
                     'products': products})
+                    
 def product_detail(request, id, slug):
     product = get_object_or_404(Product,
                                 id = id,
                                 slug = slug,
                                 available = True)
     return render(request,
-                    'shop/product/detail.html',
+                    'product/detail.html',
                     {'product': product})
